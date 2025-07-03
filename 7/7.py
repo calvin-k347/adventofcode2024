1a7d44de2c9890432a23aa5a90a9a40f613f1ce5
calvin-k347 <clark.calvink@gmail.com>
2025-07-02 22:58:31 -0400

    bit mask inspired by Neil Thistlethwaite

--- 7/7.py
+++ 7/7.py
@@ -1,16 +1,23 @@
 import re 
-
+total = 0
 with open("7pt1.txt") as f:
     for line in f:
         target = int(line[0:line.index(":")])
         nums = [int(n) for n in line[line.index(":")+1:].split()]
-        running = 1
-        print(nums)
-        for n in nums:
-            running = running * n
-            if running > target:
-                running /= n 
-                running+= n 
-        print(running, " vs ", target)
+        n = len(nums)
+        good = False
+        for i in range(1 << n - 1):
+            running = nums[0]
+            for j in range(1, n):
+                op = (i >> (j-1)) & 1
+                if op == 1:
+                    running += nums[j]
+                else:
+                    running *= nums[j]
+            if running == target:
+                good = True
+                break
+        total += good * running
 
+    print(total)
         
